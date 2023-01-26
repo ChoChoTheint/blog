9383c6264fb7fb4b023505b47eac2f19284d2127
Cho Cho Theint <chochotheint19@gmail.com>
2023-01-26 10:47:18 +0630

    26Jan

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,13 +2,38 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+
 
 class Blog{
+    public $title;
+    public $filename;
+    public $intro;
+    public $body;
+
+    public function __construct($title,$filename,$intro,$body)
+    {
+        $this->title=$title;
+        $this->filename=$filename;
+        $this->intro=$intro;
+        $this->body=$body;
+    }
     public static function all(){
         $files = File::files(resource_path("blogs"));
-       return array_map(function($file){
-           return $file->getContents();
-        },$files);
+        $blogs=[];
+        foreach($files as $file){
+        $obj = YamlFrontMatter::parseFile($file);
+        $blog = new Blog($obj->title,$obj->filename,$obj->intro,$obj->body());
+        $blogs[] = $blog;
+        // dd($obj);
+       
+        }
+        return $blogs;
+
+    //    return array_map(function($file){
+    //        return $file->getContents();
+    //     },$files);
     }
     public static function find($filename){
         $path = resource_path("/blogs/$filename.html");
