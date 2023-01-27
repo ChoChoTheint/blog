b084b3b333a990e70746f5ec76205c35120ffbed
Cho Cho Theint <chochotheint19@gmail.com>
2023-01-27 21:38:06 +0630

    27Jan night

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,19 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php foreach($blogs as $blog): ?>
-        <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a> <?= $blog->date; ?></h1>
+@extends('layout')
+
+@section('title')
+<title>All Blogs</title>
+@endsection
+
+
+@section('content')
+@foreach($blogs as $blog)
+        <h1>
+            <a href="blogs/{{ $blog->slug }}">{{ $blog->title }}</a> 
+            {{ $blog->date }}
+        </h1>
         
         <div>
-            <p><?= $blog->body; ?></p>
-
+            <p>{{ $blog->body }}</p>
         </div>
-    <?php endforeach; ?>
-</body>
-</html>
+@endforeach
+@endsection
