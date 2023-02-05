487c671d142eaaed350a42a1809a4c7eaaf25c6a
Cho Cho Theint <chochotheint19@gmail.com>
2023-02-05 20:31:10 +0630

    5Fec

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <x-hero></x-hero>
-    <x-blog-section blogs="$blogs"></x-blog-section>
+    <x-blog-section :blogs="$blogs"></x-blog-section>
     <x-subscribe></x-subscribe>
 </x-layout>
