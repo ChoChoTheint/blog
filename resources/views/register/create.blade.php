5affe2f2ce5c56de4cf43c235903c979c9a31cb7
Cho Cho Theint <chochotheint19@gmail.com>
2023-02-13 15:05:40 +0630

    13Fec

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,22 +5,36 @@
     <div class="col-md-5 mx-auto">
     <h3 class="text-primary my-3 text-center">Register Form</h3>
             <div class="card p-4 my-3 shadow-sm">
-            <form>
+            <form action="/register" method="POST">
+                @csrf
                 <div class="form-group mb-3">
                     <label for="exampleInputEmail1">Name</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="text" name="name" value="{{old('name')}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    @error('name')
+                    <p class="text-danger my-2">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="form-group mb-3">
                     <label for="exampleInputEmail1">Username</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="text" name="username" value="{{old('username')}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    @error('username')
+                        <p class="text-danger my-2">{{$message}}</p>
+                    @enderror
+                        
                 </div>
                 <div class="form-group mb-3">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" name="email" value="{{old('email')}}"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    @error('email')
+                       <p class="text-danger my-2">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="form-group mb-3">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    @error('password')
+                        <p class="text-danger my-2">{{$message}}</p>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Blog;
+use Illuminate\Http\Request;
 
 use function Ramsey\Uuid\v1;
 
@@ -24,3 +25,16 @@ Route::get('/',[BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}',[BlogController::class,'show'])->where('blog','[A-z\0-9\-]+');
 Route::get('/categories/{category:slug}',[CategoryController::class,'index'])->where('id','[A-z\d\-_]+');
 Route::get('/register',[AuthController::class,'create']);
+
+Route::post('/register',function(Request $request){
+    request()->validate([
+        'name' => 'required',
+        'username' => 'required',
+        'email' => ['required','email'],
+        'password' => 'required',
+    ],[
+        'email.required' => 'We need to know your email address!',
+        'email.email' => 'Your email is not a valid email address!',
+    ]);
+    dd('hit');
+});
